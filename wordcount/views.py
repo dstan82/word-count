b4eb597d8a3b9c4f105fda4e4382ab3a2b60f0ed
Kstorm <standragos82@gmail.com>
2020-07-13 17:19:59 +0300

    added first HTML file (home)

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def homepage(request):
-    return HttpResponse('Hello')
+    return render(request, 'home.html', {'hithere':'this is a dict'})
 
 
 def contact(request):
