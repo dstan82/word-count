daf641e28f4169dddf0f7f8f8d7449746c8a44f7
Kstorm <standragos82@gmail.com>
2020-07-13 18:53:32 +0300

    challenge done

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -18,7 +18,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.homepage),
+    path("", views.homepage, name='homepage'),
     path("count/", views.count, name='count'),
+    path("about/", views.about, name='about'),
+
 
 ]
